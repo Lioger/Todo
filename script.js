a81212aef768726c9d422a8265f8788c08dde791
Nick <nikstpnov@gmail.com>
2020-10-07 11:57:33 +0300

    refactoring of AddNewTask

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 const newTaskTextForm = document.newTaskTextForm;
 const taskList = document.querySelector('#taskList');
-let tasksCount = 0;
 
-newTaskTextForm.addEventListener('submit', addNewTask);
+newTaskTextForm.addEventListener('submit', addNewtask);
 
-function addNewTask(event){
-    event.preventDefault();
-    tasksCountIncrease();
-    const newTaskText = newTaskTextForm.newTaskText;
-    taskList.innerHTML += `<li class="task task--created" name="newTask${tasksCount}">
+
+function addNewtask(e) {
+    e.preventDefault();
+    const template = generateTemplate(e.target.addTaskInput.value);
+    taskList.innerHTML += template;
+    e.target.addTaskInput.value = '';
+}
+
+
+function generateTemplate(value){
+   const template = `<li class="task task--created" name="newTask">
         <div class="checkbox"></div>
-            <label for="toggle" class="task__text-container">${newTaskText.value}</label>
+            <label for="toggle" class="task__text-container">${value}</label>
         <div id="xMark" class="cross-container">
             <img class="image cross-image" src="./_images/cross.png" alt="close cross">
         </div>
-    </li>`
-    newTaskText.value = '';
-    const xMark = document.getElementById('xMark');
-    xMark.addEventListener('click', removeTask);
-    console.log(tasksCount);
+    </li>`;
+    return template;
 }
 
 function removeTask(){
     taskList.removeChild(`newTask${tasksCount}`);
-}
-
-function tasksCountIncrease() {
-    tasksCount += 1;
-}
-
-function tasksCountReduce() {
-    tasksCount -= 1;
 }
 
 // function showFooter() {
