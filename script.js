8f3b82af53025e3dcaa36d5b637d980d6b472d1d
Nick <nikstpnov@gmail.com>
2020-10-07 17:13:58 +0300

    Add complete icon

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
 const newTaskTextForm = document.newTaskTextForm;
 const taskList = document.querySelector('#taskList');
 const footer = document.querySelector('#footer');
+const completeAllArrow = document.querySelector('.arrow-container');
+const counterWrapper = document.querySelector('.task-counter');
 
 
 newTaskTextForm.addEventListener('submit', addNewtask);
-taskList.addEventListener('click', removeTask);
+taskList.addEventListener('click', taskAbilities);
 
 
 function generateTemplate(value){
-    const template = `<li class="task task--created" id="newTask">
+    const template = `<li class="task">
         <div class="checkbox"></div>
-            <label for="toggle" class="task__text-container">${value}</label>
-        <div class="cross-container">
-            <img name="xMark" class="image cross-image" src="./_images/cross.png" alt="close cross">
-        </div>
+        <p name="taskText" class="text-container">${value}</p>
+        <div class="cross"></div>
     </li>`;
     return template;
 }
 
 function addNewtask(e) {
     e.preventDefault();
-    const template = generateTemplate(e.target.addTaskInput.value);
-    taskList.innerHTML += template;
-    e.target.addTaskInput.value = '';
-    showFooter();
-}
-
-function removeTask(e){
-    if (e.target.classList.contains('cross-image')) {
-        e.target.parentNode.parentNode.remove();
-        showFooter();
+    if (e.target.addTaskInput.value !== '') {
+        const template = generateTemplate(e.target.addTaskInput.value);
+        taskList.innerHTML += template;
+        e.target.addTaskInput.value = '';
+        updateCounter();
+        showContent();
     }
 }
 
-function showFooter() {
-    let counter = taskList.children.length;
-    if (counter > 0) {
+function taskAbilities(e) {
+    if (e.target.classList.contains('cross')) {
+        removeTask(e);
+    } else if (e.target.classList.contains('task')) {
+        completeTask(e);
+    }
+}
+
+function removeTask(e) { 
+    e.target.parentNode.remove();
+    updateCounter();
+    showContent();
+}
+
+function completeTask(e) {
+    e.target.children[0].classList.toggle('checked-checkbox');
+    e.target.children[1].classList.toggle('completed-task');
+}
+
+function showContent() {
+    if (taskList.children.length > 0) {
+        completeAllArrow.classList.remove('hidden');
         footer.classList.remove('hidden');
     } else {
+        completeAllArrow.classList.add('hidden');
         footer.classList.add('hidden');
     }
-    const counterWrapper = document.querySelector('.task-counter');
+}
+
+function updateCounter() {
+    let counter = taskList.children.length;
     counterWrapper.textContent = `${counter} item${(counter > 1)?'s':''} left`;
 }
